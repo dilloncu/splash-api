8e7c0f5b23d71b272ae197722753568aae967ae7
Dillon Cushman <83660406+dilloncu@users.noreply.github.com>
2023-07-27 14:13:08 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./App.css";
+
+import axios from "axios";
+
+function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [img, setImg] = useState("");
+
+  function handleSearch(event) {
+    setSearchQuery(event.target.value);
+  }
+
+  async function getImage() {
+    try {
+      const API = `http://localhost:3000/photos?subject=${searchQuery}`;
+      const res = await axios.get(API);
+      // console.log(res.data);
+      setImg(res.data[0].img_url);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <div className="App">
+      <h1>Find any image</h1>
+
+
+
+      <input type="text" placeholder="enter image subject" onChange={handleSearch} />
+      <button onClick={getImage}>Explore!</button>
+      {img && <img src={img} alt={searchQuery} />}
+    </div>
+  );
+}
+
+export default App;
